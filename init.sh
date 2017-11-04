d62df7a898db104e9a80bab5f3f3dd434ed59893
terri <thuynh1989@gmail.com>
2017-11-03 22:47:40 -0700

    updating docker to read current files to docker-machine

--- init.sh
+++ init.sh
@@ -3,4 +3,4 @@ docker-machine --storage-path "/tmp/docker" create -d virtualbox camagru-docker
 # set up the environment
 eval $(docker-machine -s "/tmp/docker" env camagru-docker)
 # create app directory on docker-machine
-docker-machine -s "/tmp/docker" scp --delta --recursive ./app/ camagru-docker:/home/docker/camagru
+docker-machine -s "/tmp/docker" scp --delta --recursive ./ camagru-docker:/home/docker/camagru
