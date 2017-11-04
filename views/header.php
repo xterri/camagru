ae18ea41bcf8313d141eb75388a9eae8c07f1355
terri <thuynh1989@gmail.com>
2017-11-04 00:54:09 -0700

    setting header, footer and main layout

---
+++ views/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!-- <?include_once('../public/admin_functions.php')?> -->
+<html>
+	<head>
+		<link href="/css/styles.css" rel="stylesheet"/>
+		<?if (isset($title)): ?>
+			<title>Camagru: <?= htmlspecialchars($title) ?></title>
+		<?else: ?>
+			<title>Camagru</title>
+		<?endif ?>
+	</head>
+	<body>
+		<div class="container">
+			<div id="top">
+				<ul class="nav" align="right">
+					<li><a href="index.php">Home</a></li>
+					<?if (!empty($_SESSION["id"])): ?>
+						<li><a href="#">Lemme Take a Selfie</a></li>
+						<li><a href="#">Account Settings</a></li>
+						<li><a href="#">Fine, Leave Me</a></li>
+					<?else: ?>
+						<li><a href="#">Log In</a></li>
+						<li><a href="#">Register</a></li>
+					<?endif ?>
+				</ul>
+			</div>
+			<div id="spacer"></div>
+			<div id="middle">
---
+++ views/main.php
@@ -0,0 +1,3 @@
+<h1>Hi Camagru</h1>
+<p><h1>Here's a cute kitty picture</h1></p>
+<p><img alt="kitty" src="https://lh4.ggpht.com/M1XTibfCgpi5pgjSDb7kXDh21N8fpn-8evzQVAX-qGFhSyArDmSuCAv1pjVp4jbAt_g=h900"/></p>
