e474b04da2ed2966ee0bc274991aa5f8edb9d61e
terri <thuynh1989@gmail.com>
2017-11-18 18:38:46 -0800

    allows user to change their email and confirm the change

---
+++ app/public/activate_email.php
@@ -0,0 +1,32 @@
+<?php
+	require("../includes/helper.php");
+	require_once("../includes/admin_functions.php");
+
+	if (!empty($_GET['username'])) {
+		// getting the values from the email activation link query
+		$username = $_GET["username"];
+		$code = $_GET["code"];
+		$email = $_GET["email"];
+
+		$results = get_user_info_by_name($username);
+		$db_code = $results['confirm_code'];
+		$user_db = $results['username'];
+		// get_id value to set session_id
+		$id = $results['id'];
+	
+		// code from url == code in db; unnecessary to check username from GET and username from db?
+		if ($code == $db_code && $username == $user_db)
+		{
+			// create 2 queries to UPDATE the user table/details
+			// Update validation to true & confirm_code to zero / NULL value
+			update_validation($username, 0, 't');
+			update_email($user_db, $email); 
+			// set session_id if successful
+			$_SESSION["id"] = $id;
+			render("success.php", ["title"=>"Email Confirmation", "message"=>"Your email has been changed."]);
+		}
+		else
+			render("error.php", ["message"=>"Username and code do not match"]);
+	}
+	render("error.php");
+?>
---
+++ app/public/email.php
@@ -0,0 +1,32 @@
+<?php
+	require("../includes/helper.php");
+	require("../includes/admin_functions.php");
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["new_email"]) || empty($_POST["pw"]))
+			render("settings.php", ["message"=>"Please fill in everything"]);
+		else {
+			$results = get_user_info_by_id($_SESSION["id"]);
+			$pw_db = $results["password"];
+			$name = $results["username"];
+			$email = $results["email"];
+			if (password_verify($_POST["pw"], $pw_db)) {
+				// send activation email to new email for confirmation
+				$new_email = $_POST["new_email"];
+				$confirm_code = rand();
+				$msg = "Click the link below to confirm the new email change
+						http://192.168.99.100:8088/public/activate_email.php?email=$new_email&username=$name&code=$confirm_code";
+				mail($email, "Confirm the Email Change", $msg, "From: donotreply@camagru.com");
+				update_validation($name, $confirm_code, 'f');
+				render("success.php", ["message"=>"An email has been sent to your previous address. Please confirm the email change."]);
+			}
+			else 
+				render("settings.php", ["message"=>"Incorrect password. Cannot change to new email"]);
+		}
+	}
+	elseif ($_SERVER["REQUEST_METHOD"] == "GET") {
+		if (!empty($_SESSION["id"]))
+			render("settings.php", ["title"=>"Settings"]);
+		render("error.php", ["message"=>"Please login to access this page"]);
+	}
+?>
